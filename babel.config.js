90013f56e0f3d3bccaa5aa8875f1f64d921fd528
Shahrukh <shahrukh@ezygentechnology.com>
2024-05-08 11:38:54 +0530

    package add

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
- // presets: ['module:@react-native/babel-preset'],
+
+  // presets: ['module:@react-native/babel-preset'],
   env: {
     production: {
       plugins: ['react-native-paper/babel'],
     },
   },
+
+  plugins: [["@babel/plugin-transform-private-methods", { "loose": true }]]
+
 };
