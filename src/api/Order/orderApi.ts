830d46fc26c9b7a4eb34ebbcdebc62d55a2aee49
Shahrukh <shahrukh@ezygentechnology.com>
2024-05-09 15:17:31 +0530

    chnages

--- src/api/Order/orderApi.ts
+++ src/api/Order/orderApi.ts
@@ -3,7 +3,7 @@ import instance from "../apiInstance";
 
 
 export const upsertBBCustomerOrder = async (payload: any) => {
-    const response = await instance.post(`/customerflow/web/upsertBBCustomerOrder`, payload);
+    const response = await instance.post(`customerflow/web/upsertBBCustomerOrder`, payload);
     return response;
 };
 
